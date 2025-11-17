e52296101fe8f0f3cb1fa95b9846c957ae519689
Kaalaayaa <kalaya.chatelain@yahoo.fr>
2025-11-17 18:11:45 +0100

    echo fixed

--- src/utils/shell_init.c
+++ src/utils/shell_init.c
@@ -10,5 +10,5 @@ void	shell_init(t_shell *shell, char **envp)
         shell->env_list = env_list_init(envp);
     shell->exit_status = 0;
     update_shlvl(shell);
-    setup_signals_prompt();  /* Set up signal handlers for the shell prompt */
+    setup_signals_prompt();
 }
